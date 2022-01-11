eea67055cd80e3a1076035e8164767c787960f8e
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2022-01-11 13:00:53 -0500

    added service and better key integration

--- src/main/kotlin/tech/nathann/daysSinceTwitter/DaysSince.kt
+++ src/main/kotlin/tech/nathann/daysSinceTwitter/DaysSince.kt
@@ -4,19 +4,58 @@ import twitter4j.Twitter
 import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
 import twitter4j.createTweet
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.*
+import java.time.format.DateTimeFormatter
 
-private val keys = Keys("./keys.txt")
 fun main() {
+    //keys
+    val (
+        consumerKey,
+        consumerSecret,
+        accessToken,
+        accessSecret,
+        dateString,
+        message
+    ) = keys("./keys.txt")
+
+    //setup twitter
     val cb = ConfigurationBuilder()
     cb.setDebugEnabled(true)
-        .setOAuthConsumerKey(keys[0])
-        .setOAuthConsumerSecret(keys[1])
-        .setOAuthAccessToken(keys[2])
-        .setOAuthAccessTokenSecret(keys[3])
+        .setOAuthConsumerKey(consumerKey)
+        .setOAuthConsumerSecret(consumerSecret)
+        .setOAuthAccessToken(accessToken)
+        .setOAuthAccessTokenSecret(accessSecret)
     val factory = TwitterFactory(cb.build())
     val twitter: Twitter = factory.instance!!
 
-    twitter.createTweet(
-        text = "Testing...part 3"
-    )
+    //read date from keys
+    val date = LocalDate.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE).atTime(LocalTime.MIDNIGHT)
+    val zoned = ZonedDateTime.of(date, ZoneId.of("US/Eastern"))
+
+    //run service
+    service(twitter, zoned, message)
 }
+
+fun service(twitter: Twitter, original: ZonedDateTime, message: String) {
+    println("Starting service!")
+    while(true) {
+        val now = ZonedDateTime.now(ZoneId.of("US/Eastern"))
+        println("Running hourly check - Hour is ${now.hour}")
+        if(now.hour == 12) {
+            println("Is noon - Trying to tweet")
+            val days = Duration.between(original, now).toDays()
+            val text = message.replace("%DAYS%", days.toString())
+
+            twitter.createTweet(
+                text = text
+            )
+        }
+        Thread.sleep(Duration.ofHours(1).toMillis())
+    }
+}
+
+fun keys(path: String): List<String> = Files.readAllLines(Path.of(path)).filter { it[0] != '#' }
+
+operator fun <E> List<E>.component6(): E = this[5] //lmao
--- src/main/kotlin/tech/nathann/daysSinceTwitter/Keys.kt
+++
@@ -1,6 +0,0 @@
-package tech.nathann.daysSinceTwitter
-
-import java.nio.file.Files
-import java.nio.file.Path
-
-class Keys(path: String): List<String> by (Files.readAllLines(Path.of(path)).filter { it[0] != '#' })
